a73ca7b3b6a1478bb896cf126d066c532a0c5d71
ilya <ilyakhlamov77@gmail.com>
2024-10-12 20:00:21 +0300

    add Filter

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,31 +1,23 @@
 import Button from '../../ui/Button'
 import CreateCabinForm from './CreateCabinForm'
 import Modal from '../../ui/Modal'
-import CabinTable from './CabinTable'
 
 function AddCabin() {
 	return (
-		<Modal>
-			<Modal.Open opens="cabin-form">
-				<Button>Add new cabin</Button>
-			</Modal.Open>
-			<Modal.Window name="cabin-form">
-				<CreateCabinForm />
-			</Modal.Window>
+		<div>
+			<Modal>
+				<Modal.Open opens='cabin-form'>
+					<Button>Add new cabin</Button>
+				</Modal.Open>
+				<Modal.Window name='cabin-form'>
+					<CreateCabinForm />
+				</Modal.Window>
 
-			{/* <Modal.Open opens='table'>
-				<Button>Show table</Button>
-			</Modal.Open>
-			<Modal.Window name='table'>
-				<CabinTable />
-			</Modal.Window> */}
-		</Modal>
-	) 
+				
+			</Modal>
+		</div>
+	)
 }
-
-
-
-
 
 // function AddCabin() {
 // 	const [isOpenModal, setIsOpenModal] = useState(false)
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,7 +1,8 @@
-import { cloneElement, createContext, useContext, useState } from 'react'
+import { cloneElement, createContext, useContext, useEffect, useRef, useState } from 'react'
 import { createPortal } from 'react-dom'
 import { HiXMark } from 'react-icons/hi2'
 import styled from 'styled-components'
+import useOutSideClick from '../hooks/useOutSideClick'
 
 const StyledModal = styled.div`
 	position: fixed;
@@ -75,11 +76,13 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
 	const { openName, close } = useContext(ModalContext)
+	const ref = useOutSideClick(close)
+
 	if (name !== openName) return null
 
 	return createPortal(
 		<Overlay>
-			<StyledModal>
+			<StyledModal ref={ref}>
 				<Button onClick={close}>
 					<HiXMark />
 				</Button>
