07e9b061a0b39198a3055e13af609ef8f58195de
ilya <ilyakhlamov77@gmail.com>
2024-10-14 19:27:17 +0300

    add  .

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -4,9 +4,10 @@ import Menus from '../../ui/Menus'
 import Empty from '../../ui/Empty'
 import useBookings from './useBookings'
 import Spinner from '../../ui/Spinner'
+import Pagination from '../../ui/Pagination'
 
 function BookingTable() {
-	const { isLoading, bookings} = useBookings()
+	const { isLoading, bookings, count } = useBookings()
 
 	if(isLoading) return <Spinner />
 	if (bookings?.length === 0) return <Empty resourceName={'bookings'} />
@@ -25,10 +26,14 @@ function BookingTable() {
 
 				<Table.Body
 					data={bookings}
+					count={count}
 					render={booking => (
 						<BookingRow key={booking.id} booking={booking} />
 					)}
 				/>
+				<Table.Footer>
+					<Pagination count={count} />
+				</Table.Footer>
 			</Table>
 		</Menus>
 	)
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,24 +1,38 @@
 import { getToday } from '../utils/helpers'
+import { PAGE_SIZE } from '../utils/constants'
+
 import supabase from './supabase'
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
 	let query = supabase
 		.from('bookings')
-		.select(
-			'*, cabins(name), guests(fullName, email)'
-		)
+		.select('*, cabins(name), guests(fullName, email)', { count: 'exact' })
 
 	// FILTER
-	if (filter !== null) query = query[filter.method || 'eq'](filter.field, filter.value)
+	if (filter !== null)
+		query = query[filter.method || 'eq'](filter.field, filter.value)
+	// SortBy
+	if (sortBy)
+		query = query.order(sortBy.field, {
+			ascending: sortBy.direction === 'asc',
+		})
+	// Pagination
+	if(page) {
+		const from = (page - 1) * PAGE_SIZE
+		const to = from + PAGE_SIZE - 1
 
-	const { data, error } = await query
+		query = query.range(from, to)
+
+	}
+
+	const { data, error, count } = await query
 
 	if (error) {
 		console.error(error)
 		throw new Error('Bookings could not be loaded')
 	}
 
-	return data
+	return { data, count }
 }
 
 export async function getBooking(id) {
