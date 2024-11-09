837a0c15938ebf15ba200fbc9381869b49568f58
ilya <ilyakhlamov77@gmail.com>
2024-11-09 14:58:08 +0300

    add .

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -9,8 +9,8 @@ import SpinnerMini from '../../ui/SpinnerMini'
 
 
 function LoginForm() {
-	const [email, setEmail] = useState('ilya@mail.com')
-	const [password, setPassword] = useState('123123')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
 	const {login, isLoading} = useLogin()
 
 	function handleSubmit(e) {
