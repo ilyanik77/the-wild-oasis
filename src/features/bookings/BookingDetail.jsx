b3e52af360bfef5c9dce993c421a7b8445133b65
ilya <ilyakhlamov77@gmail.com>
2024-11-09 14:48:04 +0300

    add change 09.11.2024

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -17,6 +17,7 @@ import Modal from '../../ui/Modal'
 import ConfirmDelete from '../../ui/ConfirmDelete'
 import { deleteBooking } from '../../services/apiBookings'
 import useDeleteBooking from './useDeleteBooking'
+import Empty from '../../ui/Empty'
 
 const HeadingGroup = styled.div`
 	display: flex;
@@ -33,6 +34,8 @@ function BookingDetail() {
 
 
 	if (isLoading) return <Spinner />
+	if (!booking) return <Empty resourceName='booking' />
+
 	const { status, id: bookingId } = booking
 
 	const statusToTagName = {
--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -2,30 +2,41 @@ import styled from 'styled-components'
 import { useRecentBookings } from './useRecentBookings'
 import Spinner from '../../ui/Spinner'
 import { useRecentStays } from './useRecentStays'
+import { useCabins } from '../cabins/useCabins'
+import Stats from './Stats'
+import SalesChart from './SalesChart'
+import DurationChart from './DurationChart'
+import TodayActivity from '../check-in-out/TodayActivity'
 
 const StyledDashboardLayout = styled.div`
 	display: grid;
 	grid-template-columns: 1fr 1fr 1fr 1fr;
-	grid-template-rows: auto 34rem auto;
+	grid-template-rows: auto auto auto;
 	gap: 2.4rem;
 `
 function DashboardLayout() {
 	const { bookings, isLoading: isLoading1 } = useRecentBookings()
-	const { stays, isLoading: isLoading2, confirmedStays } = useRecentStays()
+	const {
+		isLoading: isLoading2,
+		confirmedStays,
+		numDays,
+	} = useRecentStays()
+	const { cabins, isLoading: isLoading3 } = useCabins()
 
-	if(isLoading1 || isLoading2) return <Spinner />
+	if(isLoading1 || isLoading2 || isLoading3) return <Spinner />
 
-	console.log(bookings);
 
 	return (
 		<StyledDashboardLayout>
-			<div>upiubpu</div>
-			<div>pino[in[n[</div>
-			<div>iuguoigiogi</div>
-			<div>kblkbbjlbj</div>
-			<div>upiubpu</div>
-			<div>pino[in[n[</div>
-			<div>iuguoigiogi</div>
+			<Stats
+				bookings={bookings}
+				confirmedStays={confirmedStays}
+				numDays={numDays}
+				cabinCount={cabins.length}
+			/>
+			<DurationChart confirmedStays={confirmedStays} />
+			<TodayActivity />
+			<SalesChart bookings={bookings} numDays={numDays} />
 		</StyledDashboardLayout>
 	)
 }
